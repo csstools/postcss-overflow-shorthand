598d5849bc145a97c10118a3c6c9d4727e93e390
Dmitry Semigradsky <semigradskyd@gmail.com>
2020-10-03 10:58:23 +0300

    Support PostCSS 8

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
-import postcss from 'postcss';
+import { list } from 'postcss';
 
 // space-separated values splitter
-const { list: { space } } = postcss
+const { space } = list
 
 // overflow shorthand property matcher
-const overflowPropertyRegExp = /^overflow$/i;
+const overflowPropertyRegExp = /^overflow/i;
 
-export default postcss.plugin('postcss-overflow-shorthand', opts => {
+function onCSSDeclaration(decl, preserve) {
+	// split the declaration values
+	const [overflowX, overflowY, ...invalidatingValues] = space(decl.value);
+
+	// if there are two values, but no invalidating values
+	if (overflowY && !invalidatingValues.length) {
+		// insert the overflow-* longhand declarations
+		decl.cloneBefore({
+			prop: `${decl.prop}-x`,
+			value: overflowX
+		});
+
+		decl.cloneBefore({
+			prop: `${decl.prop}-y`,
+			value: overflowY
+		});
+
+		// conditionally remove the original declaration
+		if (!preserve) {
+			decl.remove();
+		}
+	}
+}
+
+const creator = opts => {
 	const preserve = 'preserve' in Object(opts) ? Boolean(opts.preserve) : true;
 
-	return root => {
-		// for each overflow declaration
-		root.walkDecls(overflowPropertyRegExp, decl => {
-			// split the declaration values
-			const [overflowX, overflowY, ...invalidatingValues] = space(decl.value);
-
-			// if there are two values, but no invalidating values
-			if (overflowY && !invalidatingValues.length) {
-				// insert the overflow-* longhand declarations
-				decl.cloneBefore({
-					prop: `${decl.prop}-x`,
-					value: overflowX
-				});
-
-				decl.cloneBefore({
-					prop: `${decl.prop}-y`,
-					value: overflowY
-				});
-
-				// conditionally remove the original declaration
-				if (!preserve) {
-					decl.remove();
-				}
+	return {
+		postcssPlugin: 'postcss-overflow-shorthand',
+		Declaration: (decl) => {
+			if (overflowPropertyRegExp.test(decl)) {
+				onCSSDeclaration(decl, preserve);
 			}
-		})
-	};
-});
+		},
+	}
+};
+
+creator.postcss = true
+
+export default creator
